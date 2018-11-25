e2d853e85341aa38f9d3db5d9dcacff5a7e0056a
pillow14 <victor.soto.droguett@gmail.com>
2018-11-25 17:33:00 -0300

    capa de datos

---
+++ RedEgresados.DAL/Personas/PersonasDAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedEgresados.DAL
+{
+    public class PersonasDAL
+    {
+        public List<Personas> ObtenerListaPersonas()
+        {
+            using (var db = new redegresados_Entities())
+            {
+                var query = from a in db.Personas.Include("Ex_Alumnos").Include("Usuarios")
+                            select a;
+
+                return query.ToList();
+            }
+        }
+    }
+}
---
+++ RedEgresados.DAL/Usuarios/UsuariosDAL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedEgresados.DAL
+{
+    public class UsuariosDAL
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="usuariosBo"></param>
+        /// <returns></returns>
+        public Usuarios ObtenerListaUsuarios(UsuariosBO usuariosBo)
+        {
+            using (var db = new redegresados_Entities())
+            {
+                var query = (from a in db.Usuarios.Include("Personas")
+                             where a.Rol == usuariosBo.rol &&
+                             a.Password == usuariosBo.password
+                             select a);
+
+                return query.FirstOrDefault();
+            }
+        }
+    }
+}
