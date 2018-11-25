7eb0070948ead393b255deeb7f87a241afe1e339
pillow14 <victor.soto.droguett@gmail.com>
2018-11-25 17:36:04 -0300

    Add files via upload

---
+++ RedEgresados.BLL/Personas/PersonasBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedEgresados.DAL;
+
+namespace RedEgresados.BLL
+{
+   public class PersonasBLL
+    {
+       PersonasDAL personasDAL = new PersonasDAL();
+
+       public List<Personas> ObtenerListaPersonas()
+       {
+           List<Personas> listaPersonas = new List<Personas>();
+           listaPersonas = personasDAL.ObtenerListaPersonas();
+           return listaPersonas;
+       }
+    }
+}
---
+++ RedEgresados.BLL/Usuarios/UsuariosBLL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedEgresados.DAL;
+
+namespace RedEgresados.BLL
+{
+    public class UsuariosBLL
+    {
+        Usuarios usuarios = new Usuarios();
+        UsuariosDAL usuarioDal = new UsuariosDAL();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="usuariosBo"></param>
+        /// <returns></returns>
+        public Usuarios ObtenerListaUsuario(UsuariosBO usuariosBo)
+        {
+            usuarios = usuarioDal.ObtenerListaUsuarios(usuariosBo);
+            return usuarios;
+
+        }
+    }
+}
